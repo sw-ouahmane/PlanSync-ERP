1e7267b57d482f2a1ae00b7e072b555a9cec2b67
sw-ouahmane <133534816+sw-ouahmane@users.noreply.github.com>
2024-10-17 15:26:43 +0100

    Add

--- app/config.py
+++ app/config.py
@@ -16,11 +16,11 @@ class Config:
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     # This will be set in your environment
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_USERNAME = os.environ.get('infos.ouahmane@gmail.com')
     # This will be set in your environment
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PASSWORD = os.environ.get('hxur bbzh nswa twdo')
     # This will be set in your environment
-    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
+    MAIL_DEFAULT_SENDER = os.environ.get('infos.ouahmane@gmail.com')
 
 
 # postgresql://plansync_erp_l4ay_user:zL0sbPgekwRt7zxBZDnjXYIaQImXkvpw@dpg-cs1ejq88fa8c73d01r9g-a.oregon-postgres.render.com/plansync_erp_l4ay
