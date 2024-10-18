b6340a2c2d0a378e17f3127e2d58352bfc59b682
sw-ouahmane <133534816+sw-ouahmane@users.noreply.github.com>
2024-10-18 17:01:02 +0100

    Add

--- app/config.py
+++ app/config.py
@@ -15,8 +15,8 @@ class Config:
     MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
     MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'True') == 'True'
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_USERNAME = os.environ.get = 'infos.ouahmane@gmail.com'
+    MAIL_PASSWORD = os.environ.get = 'hxur bbzh nswa twdo'
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
 
     DEBUG = True
